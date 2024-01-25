58eb2bda2749e7fc879a1396d0f4866f4cc69ea8
markus_032 <64749133+markshchupak@users.noreply.github.com>
2024-01-25 23:56:51 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+ $('.fairy-tail__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+     autoplay: true,
+     fade: true
+ });
+ $('.ourtrip__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+     autoplay: true,
+     fade: true,
+     responsive: [
+        {
+          breakpoint: 601,
+          settings: {
+            arrows: false,
+          }
+        },
+      ]
+    });
+                    
+ });
